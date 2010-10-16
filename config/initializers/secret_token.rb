b68d45e77032a2bf4c658de6d01619c776d279c2
Daniel Heath <danielhe@administrators-MacBook-Pro-3.local>
2010-10-16 11:43:49 +1100

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Amped::Application.config.secret_token = '31949b677d04a9f4afa50531f90dc20c442a0761d39540f526bdb239f5d3555ac156865bfed6377499c9835d6f61fd88d3f5bf0fdc7b79cf60bd31ffe72cccbe'
